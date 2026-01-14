e4e9136e64739b5888c31bd6a9c74b88140914e3
patatavolc <edermorosrodero@gmail.com>
2026-01-14 16:21:33 +0100

    backend-feat: implement syncPriceByCardId service and updatePrice controller for price synchronization

---
+++ backend/src/controllers/price.controller.js
@@ -0,0 +1,23 @@
+import { syncPriceByCardId } from "../services/price.service.js";
+
+export const updatePrice = async (req, res) => {
+  const { cardId } = req.params;
+
+  try {
+    const price = await syncPriceByCardId(cardId);
+
+    if (!price) {
+      return res
+        .status(404)
+        .json({ message: "No se encontro precio disponible" });
+    }
+
+    res.status(200).json({
+      message: "Precio actualizado correctamente",
+      cardId,
+      currentPrice: price,
+    });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
--- backend/src/services/price.service.js
+++ backend/src/services/price.service.js
@@ -50,3 +50,41 @@ export const updateCardPrice = async (cardId) => {
     console.error(`Error actualizando precio de ${card.id}:`, error.message);
   }
 };
+
+export const syncPriceByCardId = async (cardId) => {
+  try {
+    // Consultar la API externa
+    const response = await fetch(`${POKEMON_TCG_API_URL}/cards/${cardId}`, {
+      headers: {
+        "X-Api-Key": process.env.POKEMON_TCG_API_KEY_URL || "",
+      },
+    });
+
+    if (!response.ok) throw new Error(`Error API Precios: ${response.status}`);
+
+    const { data } = await response.json();
+
+    // Extraer el precio de TCGPlayer (Market Price)
+    // Intentamos obtener el precio 'holofoil', si no 'normal'
+    const prices = data.tcgplayer?.prices;
+    const marketPrice = prices?.holofoil?.market || prices?.normal?.market || 0;
+
+    if (marketPrice === 0) {
+      console.log(`No se encontró precio de mercado para ${cardId}`);
+      return null;
+    }
+
+    // 3. Insertar en el historial
+    const sql = `
+      INSERT INTO price_history (card_id, price, source)
+      VALUES ($1, $2, $3)
+    `;
+
+    await query(sql, [cardId, marketPrice, "tcgplayer"]);
+
+    return marketPrice;
+  } catch (error) {
+    console.error(`Error en syncPriceByCardId (${cardId}):`, error.message);
+    throw error;
+  }
+};
