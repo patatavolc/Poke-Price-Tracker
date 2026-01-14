58a3b1feaeba2c9dc0d4ff14a83841beabf689d8
patatavolc <edermorosrodero@gmail.com>
2026-01-14 16:08:57 +0100

    backend-feat: fix getCardDetails controller to properly handle card retrieval and error responses

--- backend/src/controllers/card.controller.js
+++ backend/src/controllers/card.controller.js
@@ -1,11 +1,17 @@
 import { query } from "../config/db.js";
+import { getCardByIdWithHistory } from "../services/card.service.js";
 
 export const getCardDetails = async (req, res) => {
   const { id } = req.params;
   try {
-    // Obtener la carta y su historial de precios
-    const cardQuery = ""
+    const cardData = await getCardByIdWithHistory(id);
+
+    if (!cardData) {
+      return res.status(404).json({ error: "Carta no encontrada" });
+    }
+
+    res.json(cardData);
   } catch (error) {
-    
+    res.status(500).son({ error: error.message });
   }
-}
+};
