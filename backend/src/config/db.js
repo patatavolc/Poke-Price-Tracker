7de87e2d17863e227de73e05853657e51ae73c36
patatavolc <edermorosrodero@gmail.com>
2026-03-27 10:59:39 +0100

    feat: Migracion de la base de datos de supabase a una local para mejorar la velocidad en las peticiones

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -6,15 +6,16 @@ dotenv.config();
 const { Pool } = pg;
 
 const pool = new Pool({
-  connectionString: process.env.DB_URL,
-  ssl: {
-    rejectUnauthorized: false, // Obligatorio para Supabase
-  },
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    database: process.env.DB_NAME,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
 });
 
 // Helper (hacer queries y loguear errores globales)
 export const query = (text, params) => {
-  return pool.query(text, params);
+    return pool.query(text, params);
 };
 
 export default pool;
